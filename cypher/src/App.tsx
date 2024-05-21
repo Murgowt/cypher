ed4dd0d6bb47a10984790ffea3ee805df0a58ed9
Gowtham Pollam <gowthampollam@Gowthams-Laptop.local>
2024-05-21 12:37:44 -0700

    Added a test change

--- cypher/src/App.tsx
+++ cypher/src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+        Usem & Pollam Solutions.
         </p>
       </div>
       <p className="read-the-docs">
