973f6e15cc7c52680eebde9d4dfc096eb248a52d
Gowtham Pollam <gpollam@ncsu.edu>
2024-06-05 21:57:04 -0500

    Testomonials half

--- cypher/src/pages/HomePage.tsx
+++ cypher/src/pages/HomePage.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 import HeroSection from "../components/organisms/HeroSection";
-
+import TestimonialSection from "../components/sections/TestimonialSection";
 import PerfectFitSection from "../components/organisms/PerfectFitSection";
 
 interface HomePageProps {}
@@ -10,7 +10,9 @@ const HomePage : FC<HomePageProps> = () =>{
     return(
         <div className="px-10 bg-[url('/images/HeroBackground.png')] bg-no-repeat tablet:p-20 desktop-p-20">
             <HeroSection/>
+            <TestimonialSection/>
             <PerfectFitSection/>
+            
         </div>
     )
 }
