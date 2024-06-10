a4b6da408a44717ad1ffc0234fe3085a894bd244
amulyausem <63394238+amulyausem@users.noreply.github.com>
2024-06-10 10:15:24 -0400

    Update axios.ts

--- cypher/src/helpers/axios.ts
+++ cypher/src/helpers/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const customAxios = axios.create({
-    baseURL: 'http://cypherbackend-env.eba-eywrames.ap-south-1.elasticbeanstalk.com/api/',
+    baseURL: 'https://cypherbackend-env.eba-eywrames.ap-south-1.elasticbeanstalk.com/api/',
     headers: {
       'Content-Type': 'application/json',
     },    
