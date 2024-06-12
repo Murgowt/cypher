e495715069d2e139cb3615e426203dc7ccad470f
upsolve-git <160215136+upsolve-git@users.noreply.github.com>
2024-06-12 10:56:10 -0400

    chnaged header and url

--- cypher/src/helpers/axios.ts
+++ cypher/src/helpers/axios.ts
@@ -2,9 +2,10 @@ import axios from "axios";
 
 
 const customAxios = axios.create({
-    baseURL: 'https://api1.upsolve.co.in/api/',
+    baseURL: 'http://cypher-env.eba-nedbtzfz.ap-south-1.elasticbeanstalk.com/api',
     headers: {
       'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
     },    
 })
 
