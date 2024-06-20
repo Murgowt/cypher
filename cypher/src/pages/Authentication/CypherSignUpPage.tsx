0945017a26c69b1f159ae67aef01faf139a8e515
upsolve-git <160215136+upsolve-git@users.noreply.github.com>
2024-06-20 18:37:46 -0400

    build error fix

--- cypher/src/pages/Authentication/CypherSignUpPage.tsx
+++ cypher/src/pages/Authentication/CypherSignUpPage.tsx
@@ -61,7 +61,6 @@ const CypherSignUpPage: FC<CypherSignUpPageProps> =()=>{
             setErrorMsg('Passwords do not match');
             return
         }
-        let {confirmPassword,...rest}=postData
         setErrorMsg('');
         navigate(ENROLLMENT_TEST,{state:postData})
         
